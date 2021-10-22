a49310a7c2b6460e5c7b7eee169cd24cc31b2f0d
Armando Ramírez <53309070+Armando122@users.noreply.github.com>
2021-10-21 23:12:05 -0500

    Agregando distribución de trabajo

--- Practica2/practica2.rkt
+++ Practica2/practica2.rkt
@@ -1,4 +1,9 @@
 #lang plai
+;;Propuesta de distribución
+;; Armando - 1. Rombo y paralelogramo, area y perimetro para esas figuras. 2.Vagon, pasjaeros.
+;; Liprandi - 1. triangulo, cuadrado, rectangulo, area y perimetro de esa fugras. 2. peso-carbon, peso-aproximado
+;;Sebastian - 1. circulo, elipse, area y permietro. 2. tren, sin-cama
+
 ;;Tipo abstracto figura que se utiliza para trabajar
 ;;con figuras geométricas
 
