c8e80a09789558feb97329d89f7dc0db90e7d430
armando122 <armandoramirezg@ciencias.unam.mx>
2021-10-22 22:18:01 -0500

    Tipo vagon listo

--- Practica2/practica2.rkt
+++ Practica2/practica2.rkt
@@ -1,6 +1,6 @@
 #lang plai
 ;;Propuesta de distribución
-;; Armando - 1. area para esas figuras. 2.Vagon, pasjaeros.
+;; Armando - 2.pasjaeros.
 ;; Liprandi - 1. triangulo, cuadrado, rectangulo, area y perimetro de esa fugras. 2. peso-carbon, peso-aproximado
 ;;Sebastian - 1. circulo, elipse, area y permietro. 2. tren, sin-cama
 
@@ -32,6 +32,20 @@
     [(paralelogramo a b h) (* b h)])
   )
 
-
-;;Función que dada una figura regrese su perímetro.
-;;perimetro: Figura -> number
+;;Tipo de dato asbtacto vagon.
+;;(vagon-simple total util) del cual se conoce la capacidad total
+;;               de pasajeros y la capacidad utilizada.
+;;(vagon-locomotora peso) del cual se conoce el peso total de
+;;               carbón utilizado.
+;;(vagon-restaurante mesas meseros) del cual se conoce el número de mesas
+;;               y meseros disponibles.
+;;(vagon-dormitorio camas) del cual se conoce el número de camas
+;;               disponibles.
+;;(vagon-carga p) del cual se conoce el peso total utilizado
+;;             utillizado por el equipaje.
+(define-type Vagon
+  [vagon-simple (total number?) (util number?)]
+  [vagon-locomotora (peso number?)]
+  [vagon-restaurante (mesas number?) (meseros number?)]
+  [vagon-dormitorio (camas number?)]
+  [vagon-carga (p number?)])
