d973332aff3701f2533bf71e7ad4aa2acd103443
armando122 <armandoramirezg@ciencias.unam.mx>
2021-10-22 21:29:12 -0500

    Rombo y paralelogramo listo

--- Practica2/practica2.rkt
+++ Practica2/practica2.rkt
@@ -4,8 +4,15 @@
 ;; Liprandi - 1. triangulo, cuadrado, rectangulo, area y perimetro de esa fugras. 2. peso-carbon, peso-aproximado
 ;;Sebastian - 1. circulo, elipse, area y permietro. 2. tren, sin-cama
 
-;;Tipo abstracto figura que se utiliza para trabajar
-;;con figuras geométricas
+;;Tipo abstracto Figura que se utiliza para trabajar
+;;con figuras geométricas.
+;;(rombo a D d) donde a, D y d son números y representan
+;;              el lado, diagonal mayor y diagonal menor.
+;;(paralelogramo a b h) donde a, b y h son números reales; y
+;;              representan los lados y altura.
+(define-type Figura
+  [rombo (a number?) (D number?) (d number?)]
+  [paralelogramo (a number?) (b number?) (h number?)])
 
 
 ;;Función que dada una figura regrese su perímetro.
