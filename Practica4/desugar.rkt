742180b3499d615387d13a3304a835adcb31b676
armando122 <armandoramirezg@ciencias.unam.mx>
2021-11-20 23:02:41 -0600

    Subiendo esqueleto Práctica 4

---
+++ Practica4/desugar.rkt
@@ -0,0 +1,10 @@
+#lang plai
+
+(require "parser.rkt")
+(require "grammars.rkt")
+
+;; Elimina el azúcar sintáctica de las expresiones FWAE.
+;; desugar: SAST → AST
+(define (desugar expr)
+  "escribe aquí tu código")
+
