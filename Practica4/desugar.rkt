3d36fc07be062ccf9867902503ba753db63b76aa
armando122 <armandoramirezg@ciencias.unam.mx>
2021-11-28 19:13:22 -0600

    desugar de withS lista

--- Practica4/desugar.rkt
+++ Practica4/desugar.rkt
@@ -6,5 +6,19 @@
 ;; Elimina el azúcar sintáctica de las expresiones FWAE.
 ;; desugar: SAST → AST
 (define (desugar expr)
-  "escribe aquí tu código")
+  (match expr
+    [(list 'withS idvalues body) (desugar-withS idvalues body)])
+  )
 
+
+;; Función auxiliar que convierte los valores de una
+;; expresión withS en funciones currificadas.
+;; desugar-withS: litof-binding SAST -> AST
+(define (desugar-withS idvalues body)
+  (match idvalues
+    [empty (desugar body)]
+    [else
+     (app (fun (binding-id (first idvalues))
+               (desugar-withS (cdr idvalues) body))
+          (binding-value (first idvalues)))])
+  )
