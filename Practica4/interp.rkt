a046ee593f2ebac221fdce2f019e3bc2251d8135
Sebastián Salazar <47226618+SebaceoBach@users.noreply.github.com>
2021-12-02 18:04:55 -0600

    Update interp.rkt

--- Practica4/interp.rkt
+++ Practica4/interp.rkt
@@ -16,4 +16,30 @@
 ;; Análisis semántico
 ;; interp: AST Env → AST-Value
 (define (interp expr env)
-   "escribe aquí tu código")
+   (type-case AST expr
+    [id (i) (lookup i env)]
+    [num (n) (numV n)]
+    [op (f args) (interp-op f (for/list ([i args]) (convers (interp i env))))]
+    [fun (p b) (closureV p b env)]
+    [app (f arg) (interp-app f arg env)]))
+
+;;Un conversor de AST-Value a AST.
+(define (convers v)
+  (type-case AST-Value v
+    [closureV (p d e) (fun p d)]
+    [numV (n) n]))
+
+;;Funcion auxiliar que nos permite interpretar las operaciones.
+(define (interp-op f lst)
+  (let ([n (length lst)])
+    (cond
+      [(= n 1) (numV (f (first lst)))]
+      [(= n 2) (numV (f (first lst) (second lst)))]
+      [else   (numV (apply f lst))])))
+
+;;Nos permite interpretar las aplicaciones de funcion.
+(define (interp-app f a env)
+  (type-case AST f
+    [fun (p b) (interp b ((reverse p) (reverse (for/list ([i a]) (interp i env))) env))]
+    [id (i) (interp (app (convers (lookup i env)) a) (convers (lookup i env)))]
+    [else (error "interapp: Esto no debio pasar")]))
