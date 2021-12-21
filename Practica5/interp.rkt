d8f3b55004883bd460d85877a172b3ef3ab80ec7
Sebastián Salazar <47226618+SebaceoBach@users.noreply.github.com>
2021-12-21 03:29:19 -0600

    Update interp.rkt

--- Practica5/interp.rkt
+++ Practica5/interp.rkt
@@ -34,13 +34,26 @@
                      (interp t env)
                      (interp e env))]
     [fun (p b) (closureV p b env)]
-    [app (func vals)
-         (let* ([f (interp func)]
-                [env (closureV-env f)]
-                [new-ds (append-params (closureV-param f) vals env env)])
-               (interp (closureV-body f) new-ds))]))
-    
-    ;;Funcion auxiliar que nos permite interpretar las operaciones.
+    [app (f arg) (interp-app f arg env)]))
+
+
+;;Funcion que nos permite agregar a al "cache de sustituciones" nuevos elementos.
+(define (def p a env)
+  (if (empty? p)
+      env
+      (aSub (car p) (car a) (def (cdr p) (cdr a) env))))
+
+
+;;Un conversor de CFWAE-Value a CFWAE.
+(define (cc v)
+  (type-case AST-Value v
+    [closureV (p d e) (fun p d)]
+    [numV (n) n]
+    [exprV (expr xp) expr]
+    [boolV (b) b]))
+
+
+;;Funcion auxiliar que nos permite interpretar las operaciones.
 (define (interp-op f lst)
   (cond
     [(equal? f +) (interp-op-num f lst)]
@@ -52,51 +65,18 @@
     [(equal? f modulo) (interp-op-num f lst)]
     [(equal? f expt) (interp-op-num f lst)]
     [else (interp-op-bool f lst)]))
-    
-    ;;Convierte CFWAE-Value a CFWAE.
-(define (cc v)
-  (type-case AST-Value v
-    [closureV (p d e) (fun p d)]
-    [numV (n) n]
-    [exprV (expr xp) expr]
-    [boolV (b) b]))
-    
-    ;;Nos permite interpretar las aplicaciones de funcion.
-(define (interp-app f a env)
-  (type-case AST f
-    [fun (p b) (interp b (def (reverse p) (reverse (for/list ([i a]) (interp i env))) env))]
-    [id (i) (interp (app (cc (lookup i env)) a) (cs (lookup i env)))]
-    [else (error "interapp: Esto no debio pasar")]))
-    
-    ;;Funcion que nos permite agregar a al "cache de sustituciones" nuevos elementos.
-(define (def p a ds)
-  (if (empty? p)
-      ds
-      (aSub (car p) (car a) (def (cdr p) (cdr a) ds))))
-      
-     
-    
-    ;;Nos ayuda a detectar si a un iF se le ha pasado un valor booleano o no.
-(define (erroriF-aux n)
-  (if (boolean? n)
-      n
-      (error "interp: Símbolo no esperado la condicional de if, no es un booleano")))
-      
-     ;;Nos permite extraer el "cache de sustitucion" de un closure.
-(define (cs v)
-  (type-case AST-Value v
-    [closureV (p d e) e]
-    [else error "cs: Esto no debio pasar"]))
-    
-    ;;Funcion auxiliar que nos permite interpretar las operaciones que devuelven numeros.
+
+
+;;Funcion auxiliar que nos permite interpretar las operaciones que devuelven numeros.
 (define (interp-op-num f lst)
   (let ([n (length lst)])
     (cond
       [(= n 1) (numV (f (first lst)))]
       [(= n 2) (numV (f (first lst) (second lst)))]
       [else   (numV (apply f lst))])))
-      
-      ;;Funcion auxiliar que nos permite interpretar las operaciones que devuelven bool.
+
+
+;;Funcion auxiliar que nos permite interpretar las operaciones que devuelven bool.
 (define (interp-op-bool f lst)
   (let ([n (length lst)])
     (cond
@@ -104,7 +84,25 @@
       [(= n 2) (boolV (f (first lst) (second lst)))]
       [else   (boolV (apply f lst))])))
 
-(define (append-params params vals ds new-ds)
-  (if (empty? params)
-      new-ds
-      (append-params (cdr params) (cdr vals) ds (aSub (car params) (interp (car vals) ds) new-ds))))
+
+;;Nos permite interpretar las aplicaciones de funcion.
+(define (interp-app f a env)
+  (type-case AST f
+    [fun (p b) (interp b (def (reverse p) (reverse (for/list ([i a]) (interp i env))) env))]
+    [id (i) (interp (app (cc (lookup i env)) a) (cs (lookup i env)))]
+    [else (error "interapp: Esto no debio pasar")]))
+
+
+;;Nos permite extraer el "cache de sustitucion" de un closure.
+(define (cs v)
+  (type-case AST-Value v
+    [closureV (p d e) e]
+    [else error "cs: Esto no debio pasar"]))
+
+
+;;Nos ayuda a detectar si a un iF se le ha pasado un valor booleano o no.
+(define (erroriF-aux n)
+  (if (boolean? n)
+      n
+      (error "interp: Símbolo no esperado la condicional de if, no es un booleano")))
+
