f2e415b0b9198198fc2084d79732d1e72abd9740
Rodrigo <godites@live.com>
2022-01-17 17:44:37 -0600

    avance Parser

--- Practica6/parser.rkt
+++ Practica6/parser.rkt
@@ -30,4 +30,37 @@
 ;; Análisis sintáctico
 ;; parse: s-expression → SAST
 (define (parse sexp)
- "completa la función")
+   (match sexp
+     ['true (boolS #t)]
+     ['false (boolS #f)]
+     ['empty (listS '())]
+     [(? symbol?) (idS sexp)]
+     [(? number?) (numS sexp)]
+     [(cons 'list values) (listS (map parse values))]
+     [(list 'if c t e) (ifS (parse c) (parse t) (parse e))]
+     [(cons 'cond cases) (condS (crearCases cases))]
+     [(list 'with idvalues body) (withS (crearBindings idvalues) (parse body))]
+     [(list 'with* idvalues body) (withS* (crearBindings idvalues) (parse body))]
+     [(list 'fun funValues body) (funS funValues (parse body))]
+     [(cons (list 'fun funValues body) args) (appS (funS funValues (parse body)) (map parse args))]
+     [(list 'rec fun body) (recS (crearBindings fun) (parse body))]
+     [(cons s args) (opS (elige s) (map parse args))]))
+
+(define (crearCases l)
+  (match l
+    ['() '()]
+    [(cons x xs) (cons (crearCondition x) (crearCases xs)) ])
+  )
+
+(define (crearCondition p)
+  (let ([x (car p)])
+    (match x
+      ['else (else-cond (parse (cadr p)))]
+      [x (condition (parse (car p)) (parse (cadr p)))])))
+
+(define (crearBindings l)
+  (match l
+    ['() '()]
+    [(cons x xs) (cons (binding (car x) (parse (cadr x))) (crearBindings xs))]
+    )
+  )
